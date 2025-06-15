c08381d8789e0416797fee8632a9e076b0c88a2d
Nerdy-Wolfie <annnjoki2070@gmail.com>
2025-06-15 15:16:06 +0300

    Update apple_cleaning.py

--- src/apple_cleaning.py
+++ src/apple_cleaning.py
@@ -1,9 +1,8 @@
 import yfinance as yf 
-import pandas as pd 
-import os
 
-data = yf.download("AAPL" , start = "2010-01-01" , end = "2024-12-31")
+data = yf.download("AAPL" , start = "2010-01-01")
+data.columns = data.columns.get_level_values(0)
 data = data.dropna()
 data = data.reset_index()
 
-data.to_csv('apple_stock.csv' , index=False)
+data.to_csv('aapl_stock.csv' , index=False)
